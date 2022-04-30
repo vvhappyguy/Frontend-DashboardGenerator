27ad99ebab2d95182c885879b3a7ca02e9065ef3
Ivan Dyakin <vvhappyguy@gmail.com>
2022-04-30 17:57:51 +0300

    Erased extra includes

--- flutter_application_1/lib/tmp.dart
+++ flutter_application_1/lib/tmp.dart
@@ -7,7 +7,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
 void main() => runApp(const MyApp());
 
